4bfb1fb2862c2c6fb2e93352fe9f357dcaf597cb
BugHunter-X <askzenai@gmail.com>
2024-12-29 19:20:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+This error occurs when using the `expo install` command to install a package that has native modules. The error message might vary, but it generally indicates that the installation process failed due to an issue with the native modules.  Here's an example:
+```
+Error: Something went wrong during the install process.  See logs for more information.  Your project may be in a bad state.  Try running `expo prebuild` or `expo start --reset-cache` before attempting to install again.
+```
